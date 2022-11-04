6aefb74314e720b9390bad0e0b19f5411a6a82ba
tarismh <taris.hadyansyah22@gmail.com>
2022-11-04 14:08:38 +0700

    Bug Fix

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME,
+    uusername: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
