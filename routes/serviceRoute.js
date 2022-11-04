36a7d78b549b78688311e3810bf38ccd76f58934
tarismh <taris.hadyansyah22@gmail.com>
2022-11-04 12:49:48 +0700

    Add verification

--- routes/serviceRoute.js
+++ routes/serviceRoute.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
-
+const verifyToken = require('../middleware/JWTverify');
 const {
     createService,
     clientDashboard
 } = require('../controller/serviceController')
 
-router.post('/createService', createService);
-router.get('/hostedService/:email', clientDashboard)
+router.post('/createService', verifyToken, createService);
+router.get('/hostedService/:email', verifyToken, clientDashboard)
 
 module.exports = router;
