7a1ddaacd4e1b9c428e61f6cfb0390a45a82e495
tarismh <taris.hadyansyah22@gmail.com>
2022-11-04 13:45:01 +0700

    Fix Verify

--- routes/serviceRoute.js
+++ routes/serviceRoute.js
@@ -1,31 +1,11 @@
 const router = require("express").Router();
-// const verifyToken = require('../middleware/JWTverify');
+const verifyToken = require('../middleware/JWTverify');
 const {
     createService,
     clientDashboard
 } = require('../controller/serviceController')
 
-router.post('/createService', authenticateToken, createService);
-router.get('/hostedService/:email', authenticateToken, clientDashboard)
-
-function authenticateToken (req, res, next) {
-    const authHeader = req.headers['authorization']
-    if(authHeader === null)
-    {
-        stats=401;
-        error='Unauthorized';
-        throw err;
-    }
-    try{
-        const token = authHeader.split(' ')[1]
-        const tokenUser = jwt.verify(token, process.env.ACCESS_TOKEN_KEY)
-        req.user = tokenUser
-        next()
-    }
-    catch(err){
-        console.log(err);
-        res.status(400).send('Invalid Token');
-    }
-}
+router.post('/createService', verifyToken, createService);
+router.get('/hostedService/:email', verifyToken, clientDashboard)
 
 module.exports = router;
