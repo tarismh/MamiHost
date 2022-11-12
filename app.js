e3d36f9a104b2fad93a051ceaacfa1139002f65c
dharma_k <noctislucius20@gmail.com>
2022-11-12 16:08:23 +0700

    update koneksi db

--- app.js
+++ app.js
@@ -1,34 +1,42 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-require('dotenv').config();
-const { sequelize } = require('./models');
+require("dotenv").config();
+const { sequelize } = require("./models");
 const path = require("path");
 const cors = require("cors");
 
-app.use(express.json())
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.listen({port: process.env.PORT}, async() => {
-    console.log('Berjalan Pada Port ' + process.env.PORT)
-    await sequelize.authenticate()
-    console.log('Terhubung Dengan Database!')
+app.listen({ port: process.env.PORT, host: process.env.HOST }, async () => {
+    console.log(
+        "Berjalan Pada Port " +
+            process.env.PORT +
+            " dan host " +
+            process.env.HOST
+    );
+    await sequelize.authenticate();
+    console.log("Terhubung Dengan Database!");
 });
 
 const corsOptions = {
     credentials: true,
     origin: true,
-  };
+};
 
-app.get('/', (req, res) => res.send('Welcome To MamiHost API'));
+app.get("/", (req, res) => res.send("Welcome To MamiHost API"));
 
 const {
-    UserRoute, PackageRoute, ServiceRoute, UserLogin
-} = require('./routes')
+    UserRoute,
+    PackageRoute,
+    ServiceRoute,
+    UserLogin,
+} = require("./routes");
 
 app.use(cors(corsOptions));
-app.use('/user', UserRoute);
-app.use('/package', PackageRoute);
-app.use('/service', ServiceRoute);
-app.use('/login', UserLogin)
+app.use("/user", UserRoute);
+app.use("/package", PackageRoute);
+app.use("/service", ServiceRoute);
+app.use("/login", UserLogin);
 
 module.exports = app;
